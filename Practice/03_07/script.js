03a98bb3f008a8bdf3fd4878b5c05126e5a2340b
Zoey Baker <zoeybaker625@gmail.com>
2023-05-15 11:01:36 -0500

    initial commit

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,40 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const closet = {
+
+    name: "Small Closet" ,
+    Volume: 120,
+    color: "white",
+    Objectnumber: 27,
+    ClosetLength: {
+        left: 10,
+        right: 10,
+
+    },
+    closetOpen: false,
+    toggleCloset: function (closetstatus) {
+
+        this.closetOpen = closetstatus;
+    },
+
+    newClosetLength: function (lengthLeft, lengthRight) {
+        this.closetLength.left = lengthLeft;
+        this.closetLength.right = lengthRight;
+      },
+    };
+
+console.log("The Small Closet object:", closet);
+console.log("The Objectnumber value:", closet.Objectnumber);
+console.log("closet length L: ", closet.ClosetLength.left);
+
+var query = "Objectnumber";
+
+console.log("The Objectnumber value:", closet [query]);
+  
+
+
+
+
+
