c2dcb6b17bca96623cc72f2b20507099b10a7477
Zoey Baker <zoeybaker625@gmail.com>
2023-05-29 13:06:28 -0500

    initial commit

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,27 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+const laptop = {
+
+    name: "Zoeys Macbook",
+    color: "silver",
+    ram: 32,
+    storagespace: 500,
+    laptoplength: {
+    left: 32,
+    right: 32,
+
+    },
+togglepower: function (powerStatus) {
+    this.poweron = powerStatus;
+  },
+  newlaptopLength: function (lengthLeft, lengthRight) 
+  {
+    this.laptopLength.left = lengthLeft;
+    this.laptopLength.right = lengthRight;
+  },
+
+};
+
+
